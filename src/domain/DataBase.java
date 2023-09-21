cb207ca429978bb852609f06a084cae4626a0a6b
samandykov <cloudkazakhstan@gmail.com>
2023-09-21 19:03:23 +0600

    Sprint 1.0

---
+++ src/domain/DataBase.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.sql.Connection;
+
+public class DataBase {
+
+    private static Connection connection;
+
+    static {
+        try{
+            Class.forName("")
+
+        } catch (Exception e){
+
+        }
+    }
+}
---
+++ src/domain/user/Users.java
@@ -0,0 +1,53 @@
+package domain.user;
+
+public class Users {
+
+    private Integer id;
+
+    private String login;
+    private String password;
+
+    private String fullName;
+
+    public Users() {
+    }
+
+    public Users(Integer id, String login, String password, String fullName) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
